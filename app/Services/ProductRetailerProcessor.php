896fd2d5be57a66036feeb5444a073fa33367bcd
Arvydas Kapacinskas <arvydas.kapacinskas@nfq.lt>
2023-06-27 15:22:11 +0300

    Move job logic into dedicated class

---
+++ app/Services/ProductRetailerProcessor.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Exceptions\ScraperNotFoundException;
+use App\Exceptions\ScrapingFailedException;
+use App\Models\ProductRetailer;
+use App\Notifications\PriceDrop;
+use App\Scraper\ScrapData;
+use App\Scraper\ScraperFactory;
+use Illuminate\Contracts\Notifications\Dispatcher as NotificationDispatcher;
+
+class ProductRetailerProcessor
+{
+    public function __construct(
+        private readonly ScraperFactory $scraperFactory,
+        private readonly ProductRetailerComparator $productRetailerComparator,
+        private readonly NotificationDispatcher $notification,
+    ) {
+    }
+
+    /**
+     * @throws ScraperNotFoundException|ScrapingFailedException
+     */
+    public function process(ProductRetailer $productRetailer, bool $notify): void
+    {
+        $product = $productRetailer->product;
+        $bestRetailer = $product->bestRetailer();
+
+        $data = $this->scraperFactory
+            ->createFromRetailer($productRetailer)
+            ->scrap($productRetailer);
+
+        $this->updateProductRetailer($productRetailer, $data);
+
+        if ($notify && $this->productRetailerComparator->isPriceLessThan($productRetailer, $bestRetailer)) {
+            $this->notification->send($product->user, new PriceDrop($productRetailer));
+        }
+    }
+
+    private function updateProductRetailer(ProductRetailer $productRetailer, ScrapData $data): void
+    {
+        $productRetailer->price = $data->getPrice();
+
+        if ($data->getCurrency() !== null) {
+            $productRetailer->currency = $data->getCurrency();
+        }
+
+        if ($data->getImage() !== null) {
+            $productRetailer->image = $data->getImage();
+        }
+
+        $productRetailer->save();
+    }
+}
